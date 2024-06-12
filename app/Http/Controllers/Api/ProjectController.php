a5dcc7d5f379f9baac9808c8dcb003711fdf4ab9
Andrea-Calligari <andrea.calligari26@gmail.com>
2024-06-12 17:41:56 +0200

    ADDED: paginate

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -7,12 +7,13 @@ use Illuminate\Http\Request;
 
 class ProjectController extends Controller
 {
-    public function index(){
+    public function index(Request $request){
 
         // $projects = Project::all();
-
+        $pages = $request->pages ?? 3;
         //prende anche i dati delle relazioni e i dati della tabella pivot
-        $projects = Project::with('type','technologies')->get();
+        $projects = Project::with('type','technologies')->paginate($pages);
+
         return response()->json([
             'projects'=> $projects,
         ]);
